ae48605e0e5fe92bc5cbcd98eceb0fd8a554b59e
Richard You <richardyou94@gmail.com>
2023-02-13 00:45:13 -0800

    trying to add date varaible to test if today is weekend or weekday

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  var today = new Date();
+
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    res.send("Yay it's the weekend!");
+  } else {
+    res.send("Boo! I have to work!");
+  }
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
