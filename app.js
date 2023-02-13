17ea472c9404fe7028efdb72f9b7e22b2d270e60
Richard You <richardyou94@gmail.com>
2023-02-13 00:56:05 -0800

    used switch to see what day is today

--- app.js
+++ app.js
@@ -3,15 +3,40 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
+  var day = "";
 
-  if (currentDay === 6 || currentDay === 0) {
-    res.write("<h1>Yay it's the weekend!</h1>");
-  } else {
-    res.sendFile(__dirname + "/index.html");
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log(`Error: current day is equal to: ${currentDay}`);
   }
+
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
