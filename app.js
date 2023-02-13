68f887d25a2dc0a3d37ffc425428f5d3d2a6ef8e
Richard You <richardyou94@gmail.com>
2023-02-13 01:16:20 -0800

    add stylesheet to todolist

--- app.js
+++ app.js
@@ -3,40 +3,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  let today = new Date();
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log(`Error: current day is equal to: ${currentDay}`);
-  }
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  res.render("list", { kindOfDay: day });
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
