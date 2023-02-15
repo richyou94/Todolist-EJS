e2929b3d574d4f8e2c1d593fb5cb3305925e6f6b
Richard You <richardyou94@gmail.com>
2023-02-15 15:09:02 -0800

    add mongoose connection

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+});
+
+
 app.get("/", function (req, res) {
-  
   let day = date.getDate();
 
   res.render("list", { listTitle: day, newListItems: items });
